7c121072d8754d94335b22d083f944edb96c98a6
Peter Hurford <peter@peterhurford.com>
2020-05-19 13:19:07 -0400

    Serve static CSS and format page some

--- link/views.py
+++ link/views.py
@@ -5,7 +5,7 @@ from link.models import Link, Upcoming
 
 class LinkListView(ListView):
     model = Link
-    paginate_by = 30
+    paginate_by = 10
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -14,7 +14,7 @@ class LinkListView(ListView):
 
 class UpcomingListView(ListView):
     model = Upcoming
-    paginate_by = 30
+    paginate_by = 15
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
--- project_stanza/urls.py
+++ project_stanza/urls.py
@@ -1,9 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+
 from link.views import LinkListView, UpcomingListView
+
 
 urlpatterns = [
     path('', UpcomingListView.as_view(), name='upcoming-list'),
     path('links/', LinkListView.as_view(), name='link-list'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
