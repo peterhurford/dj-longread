6de7573d9661b42c887537e669d6a7741164c2ce
Peter Hurford <peter@peterhurford.com>
2020-05-24 02:21:28 -0400

    Space

--- link/admin.py
+++ link/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 
 from .models import Link, Upcoming
 
-admin.site.register(Link)
-admin.site.register(Upcoming)
+
+class LinkAdmin(admin.ModelAdmin):
+    search_fields = ('title', 'summary', 'category', 'aggregator')
+
+
+class UpcomingAdmin(admin.ModelAdmin):
+    search_fields = ('title', 'aggregator')
+
+
+admin.site.register(Link, LinkAdmin)
+admin.site.register(Upcoming, UpcomingAdmin)
