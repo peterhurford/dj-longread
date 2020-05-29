a72ed8e7a782980df0c81cabf411eb7a39ccbf64
Peter Hurford <peter@peterhurford.com>
2020-05-28 22:49:45 -0400

    Fix URL util

--- link/utils/url.py
+++ link/utils/url.py
@@ -1,8 +1,13 @@
+# TODO: DRY with utils/url.py
+
 from urllib.parse import urlparse
 
 def clean_url(url):
-    url = 'http' + url.split('http')[1]
-    return url
+    if 'http' not in url:
+        return 'http:' + url
+    else:
+        url = 'http' + url.split('http')[1]
+        return url
 
 def get_root_url(url):
     return urlparse(url).netloc
