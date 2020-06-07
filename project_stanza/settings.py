44a03ab400cf9871b41418c166fabedb0b8ced8e
Peter Hurford <peter@peterhurford.com>
2020-06-07 16:08:08 -0400

    Add logging

--- project_stanza/settings.py
+++ project_stanza/settings.py
@@ -109,3 +109,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+}
+
