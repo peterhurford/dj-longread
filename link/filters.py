6f08ad073dbdecd9c5711a3322f6c00a033dc0dd
Peter Hurford <peter@peterhurford.com>
2020-06-15 20:45:37 -0400

    rm filters

--- link/filters.py
+++
@@ -1,7 +0,0 @@
-from link.models import Link
-import django_filters
-
-class LinkFilter(django_filters.FilterSet):
-    class Meta:
-        model = Link
-        fields = ['url', 'title', 'summary', 'liked', 'category', 'aggregator']
