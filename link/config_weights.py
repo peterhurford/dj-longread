3b8fdc36ebcbc714de329ef6dc52a7a1f3bc3ecd
Peter Hurford <peter@peterhurford.com>
2020-08-27 19:52:57 -0500

    Move weights to a config file

---
+++ link/config_weights.py
@@ -0,0 +1,50 @@
+from django.db.models import Case, When, Q, FloatField
+
+
+PRIORITY_WEIGHT = 17  # The lower this number, the more links will be ranked according
+                      # to the manual preferences set in `AGGREGATOR_WEIGHTS` below.
+
+TIME_WEIGHT = 15      # The lower this number, the more it will be the case that recent
+                      # links show up first
+
+RANDOM_WEIGHT = 40    # The lower this number, the more it will be the case that links will
+                      # show up in a random order, disregarding recenty or aggregator weights
+
+# The relative rankings of different aggregators
+AGGREGATOR_WEIGHTS = Case(When(Q(aggregator__exact='LFaA'), then=40),
+                          When(Q(aggregator__exact='FPMorning'), then=40),
+                          When(Q(aggregator__exact='FPSecurity'), then=40),
+                          When(Q(aggregator__exact='FPChina'), then=40),
+                          When(Q(aggregator__exact='FPSouthAsia'), then=40),
+                          When(Q(aggregator__exact='FP-WYWL'), then=40),
+                          When(Q(aggregator__exact='MorningAg'), then=40),
+                          When(Q(aggregator__exact='Dispatch') &
+                               Q(title__startswith='The Morning'), then=40),
+                          When(Q(aggregator__exact='Custom'), then=25),
+                          When(Q(aggregator__exact='EAForum'), then=14),
+                          When(Q(aggregator__exact='538'), then=14),
+                          When(Q(aggregator__exact='SSC'), then=14),
+                          When(Q(aggregator__exact='80K'), then=14),
+                          When(Q(aggregator__exact='CEA'), then=14),
+                          When(Q(aggregator__exact='Noah'), then=8),
+                          When(Q(aggregator__exact='AI Impacts'), then=8),
+                          When(Q(aggregator__exact='DanWahl'), then=8),
+                          When(Q(aggregator__exact='JenSkerritt'), then=8),
+                          When(Q(aggregator__exact='Lusk'), then=8),
+                          When(Q(aggregator__exact='Leo'), then=8),
+                          When(Q(aggregator__exact='NMA'), then=8),
+                          When(Q(aggregator__exact='NerdFitness'), then=8),
+                          When(Q(aggregator__exact='ALOP'), then=8),
+                          When(Q(aggregator__exact='Vox'), then=7),
+                          When(Q(aggregator__exact='Dispatch'), then=7),
+                          When(Q(aggregator__exact='CurrentAffairs'), then=6),
+                          When(Q(aggregator__exact='LW'), then=6),
+                          When(Q(aggregator__exact='Caplan'), then=6),
+                          When(Q(aggregator__exact='MOF'), then=6),
+                          When(Q(aggregator__exact='MR'), then=6),
+                          When(Q(aggregator__exact='Rosewater'), then=6),
+                          When(Q(aggregator__exact='Sumner'), then=4),
+                          When(Q(aggregator__exact='D4P'), then=4),
+                          When(Q(aggregator__exact='3P'), then=3),
+                          default=1,
+                          output_field=FloatField())
--- link/views.py
+++ link/views.py
@@ -2,17 +2,15 @@ import logging
 
 from datetime import datetime
 
-from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView
-from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
-from django.db.models import Case, When, Q, F, FloatField, ExpressionWrapper, Max, Count
-from django.shortcuts import render
+from django.db.models import Q, F, ExpressionWrapper, FloatField
 from django.utils import timezone
 
 from link.models import Link
 
 from .utils.url import clean_url, get_root_url
+from .config_weights import PRIORITY_WEIGHT, TIME_WEIGHT, RANDOM_WEIGHT, AGGREGATOR_WEIGHTS
 
 
 class LinkListView(ListView):
@@ -102,47 +100,10 @@ class UpcomingListView(ListView):
         elif sort == 'diverseoldest':
             queryset = queryset.order_by('seed', 'added')
         else:
-            queryset = (queryset.annotate(priority=Case(
-                                            When(Q(aggregator__exact='LFaA'), then=40),
-                                            When(Q(aggregator__exact='FPMorning'), then=40),
-                                            When(Q(aggregator__exact='FPSecurity'), then=40),
-                                            When(Q(aggregator__exact='FPChina'), then=40),
-                                            When(Q(aggregator__exact='FPSouthAsia'), then=40),
-                                            When(Q(aggregator__exact='FP-WYWL'), then=40),
-                                            When(Q(aggregator__exact='MorningAg'), then=40),
-                                            When(Q(aggregator__exact='Dispatch') &
-                                                 Q(title__startswith='The Morning'), then=40),
-                                            When(Q(aggregator__exact='Custom'), then=25),
-                                            When(Q(aggregator__exact='EAForum'), then=14),
-                                            When(Q(aggregator__exact='538'), then=14),
-                                            When(Q(aggregator__exact='SSC'), then=14),
-                                            When(Q(aggregator__exact='80K'), then=14),
-                                            When(Q(aggregator__exact='CEA'), then=14),
-                                            When(Q(aggregator__exact='Noah'), then=8),
-                                            When(Q(aggregator__exact='AI Impacts'), then=8),
-                                            When(Q(aggregator__exact='DanWahl'), then=8),
-                                            When(Q(aggregator__exact='JenSkerritt'), then=8),
-                                            When(Q(aggregator__exact='Lusk'), then=8),
-                                            When(Q(aggregator__exact='Leo'), then=8),
-                                            When(Q(aggregator__exact='NMA'), then=8),
-                                            When(Q(aggregator__exact='NerdFitness'), then=8),
-                                            When(Q(aggregator__exact='ALOP'), then=8),
-                                            When(Q(aggregator__exact='Vox'), then=7),
-                                            When(Q(aggregator__exact='Dispatch'), then=7),
-                                            When(Q(aggregator__exact='CurrentAffairs'), then=6),
-                                            When(Q(aggregator__exact='LW'), then=6),
-                                            When(Q(aggregator__exact='Caplan'), then=6),
-                                            When(Q(aggregator__exact='MOF'), then=6),
-                                            When(Q(aggregator__exact='MR'), then=6),
-                                            When(Q(aggregator__exact='Rosewater'), then=6),
-                                            When(Q(aggregator__exact='Sumner'), then=4),
-                                            When(Q(aggregator__exact='D4P'), then=4),
-                                            When(Q(aggregator__exact='3P'), then=3),
-                                            default=1,
-                                            output_field=FloatField()))
-                        .annotate(priority=ExpressionWrapper((1 + (F('priority') / 18.0)) +
-                                                                  (F('id') / 2000.0) +
-                                                                  (F('seed') / 40.0),
+            queryset = (queryset.annotate(priority=AGGREGATOR_WEIGHTS)
+                        .annotate(priority=ExpressionWrapper((1 + (F('priority') / PRIORITY_WEIGHT)) +
+                                                                  (F('id') / (TIME_WEIGHT * 100)) +
+                                                                  (F('seed') / RANDOM_WEIGHT),
                                                              output_field=FloatField()))
                         .order_by('-priority'))
         queryset = queryset.all()
