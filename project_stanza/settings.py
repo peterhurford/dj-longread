7badc4ee63ddb96511502509417a440dd5561a70
Peter Hurford <peter@peterhurford.com>
2020-10-05 16:27:08 -0500

    Format link list

--- project_stanza/settings.py
+++ project_stanza/settings.py
@@ -23,6 +23,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'link',
+    'link.templatetags.link_extras',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
