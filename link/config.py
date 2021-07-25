dabe6b005dc995cc01e7c356749d21e430e43650
Peter Hurford <peter@peterhurford.com>
2021-07-25 16:20:17 -0500

    Change and upweight aggregators

--- link/config.py
+++ link/config.py
@@ -11,7 +11,7 @@ OBSOLETE_AGGREGATORS = ['MR', 'HN', 'EABlogs', 'Reason', 'NR', 'Quillette', 'Vox
 
 
 # Metaweights
-PRIORITY_WEIGHT = 16  # The lower this number, the more links will be ranked according
+PRIORITY_WEIGHT = 14  # The lower this number, the more links will be ranked according
                       # to the manual preferences set in `AGGREGATOR_WEIGHTS` below.
 
 TIME_WEIGHT = 8       # The lower this number, the more it will be the case that recent
@@ -26,24 +26,25 @@ AGGREGATOR_WEIGHTS = Case(When(Q(aggregator__exact='Dispatch') &
                                Q(title__startswith='The Morning'), then=100),
                           When(Q(aggregator__exact='Custom'), then=20),
                           When(Q(aggregator__exact='HIPR'), then=12),
-                          When(Q(aggregator__exact='SLW'), then=10),
+                          When(Q(aggregator__exact='SLW'), then=8),
+                          When(Q(aggregator__exact='SuperOrganizers'), then=8),
                           When(Q(aggregator__exact='EAForum'), then=8),
                           When(Q(aggregator__exact='Bollard'), then=8),
-                          When(Q(aggregator__exact='80K'), then=8),
-                          When(Q(aggregator__exact='CEA'), then=8),
                           When(Q(aggregator__exact='SSC'), then=8),
                           When(Q(aggregator__exact='FPMorning'), then=8),
                           When(Q(aggregator__exact='FPSecurity'), then=8),
                           When(Q(aggregator__exact='FPChina'), then=8),
                           When(Q(aggregator__exact='FPSouthAsia'), then=8),
                           When(Q(aggregator__exact='FP-WYWL'), then=8),
-                          When(Q(aggregator__exact='GlobalGuessing'), then=8),
+                          When(Q(aggregator__exact='GlobalGuessing'), then=6),
                           When(Q(aggregator__exact='AI Impacts'), then=6),
                           When(Q(aggregator__exact='Alignment'), then=6),
                           When(Q(aggregator__exact='ImportAI'), then=6),
-                          When(Q(aggregator__exact='Lusk'), then=4),
-                          When(Q(aggregator__exact='ScholarsSage'), then=4),
-                          When(Q(aggregator__exact='PoliticalKiwi'), then=4),
+                          When(Q(aggregator__exact='Lusk'), then=6),
+                          When(Q(aggregator__exact='ScholarsSage'), then=6),
+                          When(Q(aggregator__exact='PoliticalKiwi'), then=6),
+                          When(Q(aggregator__exact='80K'), then=6),
+                          When(Q(aggregator__exact='CEA'), then=6),
                           When(Q(aggregator__exact='Holden'), then=4),
                           default=1,
                           output_field=FloatField())
