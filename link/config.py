df6fa5de741f90274c1a3202dd27e7d9156a3908
Peter Hurford <peter@peterhurford.com>
2021-07-31 14:12:49 -0400

    Upweight ChinAI / ImportAI / Alignment newsletters and obsolete them

--- link/config.py
+++ link/config.py
@@ -2,7 +2,7 @@ from django.db.models import Case, When, Q, FloatField
 
 # Purge old articles if they come from these aggregators
 PURGABLE_AGGREGATORS = ['Dispatch', 'LFaA', 'FPMorning', 'FPSecurity', 'FPChina', 'FPSouthAsia',
-                        'FP-WYWL', 'MorningAg', '538', 'ChinAI']
+                        'FP-WYWL', 'MorningAg', '538', 'ChinAI', 'ImportAI', 'Alignment']
 PURGE_OLDER_THAN_X_DAYS = 5  # For PURGABLE_AGGREGATORS, remove links older than this (in days)
 
 
@@ -29,20 +29,21 @@ AGGREGATOR_WEIGHTS = Case(When(Q(aggregator__exact='Dispatch') &
                                Q(title__startswith='The Morning'), then=100),
                           When(Q(aggregator__exact='Custom'), then=20),
                           When(Q(aggregator__exact='HIPR'), then=12),
-                          When(Q(aggregator__exact='SLW'), then=8),
-                          When(Q(aggregator__exact='SuperOrganizers'), then=8),
-                          When(Q(aggregator__exact='EAForum'), then=8),
-                          When(Q(aggregator__exact='Bollard'), then=8),
-                          When(Q(aggregator__exact='SSC'), then=8),
-                          When(Q(aggregator__exact='FPMorning'), then=8),
-                          When(Q(aggregator__exact='FPSecurity'), then=8),
-                          When(Q(aggregator__exact='FPChina'), then=8),
-                          When(Q(aggregator__exact='FPSouthAsia'), then=8),
-                          When(Q(aggregator__exact='FP-WYWL'), then=8),
+                          When(Q(aggregator__exact='SLW'), then=10),
+                          When(Q(aggregator__exact='SuperOrganizers'), then=10),
+                          When(Q(aggregator__exact='EAForum'), then=10),
+                          When(Q(aggregator__exact='Bollard'), then=10),
+                          When(Q(aggregator__exact='SSC'), then=10),
+                          When(Q(aggregator__exact='FPMorning'), then=10),
+                          When(Q(aggregator__exact='FPSecurity'), then=10),
+                          When(Q(aggregator__exact='FPChina'), then=10),
+                          When(Q(aggregator__exact='FPSouthAsia'), then=10),
+                          When(Q(aggregator__exact='FP-WYWL'), then=10),
+                          When(Q(aggregator__exact='ChinAI'), then=10),
+                          When(Q(aggregator__exact='ImportAI'), then=10),
+                          When(Q(aggregator__exact='Alignment'), then=10),
                           When(Q(aggregator__exact='GlobalGuessing'), then=6),
                           When(Q(aggregator__exact='AI Impacts'), then=6),
-                          When(Q(aggregator__exact='Alignment'), then=6),
-                          When(Q(aggregator__exact='ImportAI'), then=6),
                           When(Q(aggregator__exact='ScholarsSage'), then=6),
                           When(Q(aggregator__exact='PoliticalKiwi'), then=6),
                           When(Q(aggregator__exact='80K'), then=6),
