a3f48a80b6c7571178ab575472c4163731814964
Peter Hurford <peter@peterhurford.com>
2021-09-23 13:49:33 -0500

    More random

--- link/config.py
+++ link/config.py
@@ -17,7 +17,7 @@ PRIORITY_WEIGHT = 10   # The lower this number, the more links will be ranked ac
 TIME_WEIGHT = 8        # The lower this number, the more it will be the case that recent
                        # links show up first
 
-RANDOM_WEIGHT = 50     # The lower this number, the more it will be the case that links will
+RANDOM_WEIGHT = 35     # The lower this number, the more it will be the case that links will
                        # show up in a random order, disregarding recenty or aggregator weights
 
 # Equation for ranking = 1 + (aggregator weight / PRIORITY_WEIGHT) +
