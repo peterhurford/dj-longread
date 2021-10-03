5ff9ddcea62ef3903f1d2a5c6e4ef26cf1acc5a5
Peter Hurford <peter@peterhurford.com>
2021-10-03 14:59:21 -0500

    fix float issue

--- ingest/import_db.py
+++ ingest/import_db.py
@@ -3,12 +3,10 @@ import psycopg2
 
 import pandas as pd
 
-from utils.sql import import_db, clean_links
+from utils.sql import import_db
 
 
 print('Psycopg2 connect...')
-links = clean_links(pd.read_csv('data/export.csv'))
-links.to_csv('data/export.csv', index=False)
 DATABASE_URL = os.environ.get('DATABASE_URL', 'dbname=stanza_dev user=dbuser')
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 import_db(conn)
--- utils/sql.py
+++ utils/sql.py
@@ -84,8 +84,8 @@ def clean_links(links):
     links.columns = ALL_COLS
     links = links[links['id'].notnull()]   # Drop empty column
     links.loc[:, 'id'] = links['id'].astype(int)  # Fix float ID issue
-    links.loc[:, 'seed'] = links['seed'].astype(int)
-    links.loc[:, 'tweet'] = links['tweet'].apply(lambda x: 0 if str(x) == '\\N' else str(x).split('.')[0]).astype(int)
+    links.loc[:, 'seed'] = links['seed'].astype(float).fillna(0)
+    links.loc[:, 'tweet'] = links['tweet'].apply(lambda x: 0 if (str(x) == '\\N' or str(x) == '') else str(x).split('.')[0]).astype(float)
     links.loc[:, 'liked'] = links['liked'].apply(lambda x: np.nan if (str(x) == '\\N' or str(x) == '') else str(x).split('.')[0]).astype(float)
     for var in ['url', 'title', 'summary', 'domain', 'category', 'aggregator']:
         links.loc[:, var] = links[var].apply(clean_str)
