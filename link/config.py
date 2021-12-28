98a6a29b08bc9a0b933d22055cc50b69ebd75d8b
Peter Hurford <peter@peterhurford.com>
2021-12-27 19:38:34 -0600

    Obsolete more aggregators

--- link/config.py
+++ link/config.py
@@ -8,7 +8,7 @@ PURGE_OLDER_THAN_X_DAYS = 5  # For PURGABLE_AGGREGATORS, remove links older than
 
 
 # Purge all articles from these aggregators
-OBSOLETE_AGGREGATORS = ['ChristianRationalist', 'Harford', 'PragCap', 'Ariely', 'EconometricSense', 'Devon', 'Seliger', 'CH', 'Niskanen', 'DR', '127', '3P', 'Ozy', 'Pseudoerasmus']
+OBSOLETE_AGGREGATORS = ['FLI', 'MMM', 'Leo', 'RyanAvent', 'NotEvenWrong']
 
 # Metaweights
 PRIORITY_WEIGHT = 10   # The lower this number, the more links will be ranked according
