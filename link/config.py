39d019aae510a6a09a2df09f56585ebbdcd58e02
Peter Hurford <peter@peterhurford.com>
2021-12-27 19:47:30 -0600

    Obsolete more aggregators VII

--- link/config.py
+++ link/config.py
@@ -8,7 +8,7 @@ PURGE_OLDER_THAN_X_DAYS = 5  # For PURGABLE_AGGREGATORS, remove links older than
 
 
 # Purge all articles from these aggregators
-OBSOLETE_AGGREGATORS = ['WBW', 'Frum', 'Gunther', 'PossiblyWrong', 'Scrimshaw']
+OBSOLETE_AGGREGATORS = ['WBW', 'Frum', 'Gunther', 'PossiblyWrong', 'Scrimshaw', 'BLH']
 
 # Metaweights
 PRIORITY_WEIGHT = 10   # The lower this number, the more links will be ranked according
