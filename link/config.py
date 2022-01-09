cbe42a0a2f2fb2d5b474ec6ebf302ccad4913678
Peter Hurford <peter@peterhurford.com>
2022-01-09 00:55:52 -0600

    Clear out more dead aggregators XV

--- link/config.py
+++ link/config.py
@@ -16,7 +16,7 @@ LONG_PURGE_OLDER_THAN_X = 30*3  # For LONG_PURGABLE_AGGREGATORS, remove links ol
 
 # Purge all articles from these aggregators
 OBSOLETE_AGGREGATORS = ['GFI', 'Eghbal', 'GWWC', 'GiveDirectly', 'HIPR', 'KenWhite',
-                        'YaschaMounk']
+                        'YaschaMounk', 'Rosie']
 
 
 # Metaweights
