c5091b04a902ca71bf97007e423875f0986e7134
Peter Hurford <peter@peterhurford.com>
2022-04-07 16:47:50 -0500

    Add War on the Rocks

--- ingest/aggregate_feeds.py
+++ ingest/aggregate_feeds.py
@@ -331,6 +331,7 @@ contents += load_contents('MattLakeman', 'https://mattlakeman.org/feed/')
 contents += load_contents('FP21',
                           'https://us18.campaign-archive.com/feed?u=75b96a50185221e3874428fc6&id=0232491d26',
                           'item')
+contents += load_contents('WarOnRocks', 'https://warontherocks.com/feed/')
 
 print('-')
 print('Gathering content')
