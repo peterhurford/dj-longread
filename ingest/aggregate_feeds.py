e99e139b1e7b149a794c139ba2c24296a16527ff
Peter Hurford <peter@peterhurford.com>
2023-04-12 18:02:14 -0400

    add SafeAI newsletter

--- ingest/aggregate_feeds.py
+++ ingest/aggregate_feeds.py
@@ -313,6 +313,7 @@ contents += load_contents('PriceTheory', 'https://pricetheory.substack.com/feed'
 contents += load_contents('Putanumonit', 'https://putanumonit.com/feed/')
 contents += load_contents('RiversInfect', 'https://caitlinrivers.substack.com/feed')
 contents += load_contents('Salonium', 'https://salonium.substack.com/feed')
+contents += load_contents('SafeAI', 'https://newsletter.safe.ai/feed')
 contents += load_contents('SamAltman', 'http://blog.samaltman.com/posts.atom', 'entry-link')
 contents += load_contents('ScholarsStage', 'https://scholars-stage.org/feed/')
 contents += load_contents('Schubert', 'https://stefanfschubert.com/blog?format=rss')
