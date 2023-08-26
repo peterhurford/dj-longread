cf276baa3d5345f3c7522befaa63c6d3352f878b
Peter Wildeford <peter@peterhuford.com>
2023-08-26 16:17:48 +0100

    change weights

--- link/config.py
+++ link/config.py
@@ -4,15 +4,12 @@ from django.db.models import Case, When, Q, FloatField
 # Purge old articles after 14 days if they come from these aggregators
 PURGABLE_AGGREGATORS = ['Dispatch', 'FPSecurity', 'FPChina', 'ChinAI', 'ImportAI',
                         'Alignment', 'TIB', 'Schneier', 'Yglesias', 'MLSafety']
-PURGE_OLDER_THAN_X_DAYS = 14  # For PURGABLE_AGGREGATORS, remove links older than this (in days)
+PURGE_OLDER_THAN_X_DAYS = 14
 
 
-# Purge old articles after 90 days if they come from these aggregators
-LONG_PURGABLE_AGGREGATORS = ['AskManager', 'SLW', 'CSET', 'Caplan',
-                             'Sumner', 'GEMorris', 'Lusk', 'TSNR', 'GlobalGuessing',
-                             'SplitTicket', 'Intelligencer', 'Guzey', 'HLI', 'EALondon',
-                             'CEA', 'AlignmnentForum', 'Zvi']
-LONG_PURGE_OLDER_THAN_X = 30*2  # For LONG_PURGABLE_AGGREGATORS, remove links older than this (in days)
+# Purge old articles after 60 days if they come from these aggregators
+LONG_PURGABLE_AGGREGATORS = ['AskManager', 'SLW', 'SplitTicket', 'Noah']
+LONG_PURGE_OLDER_THAN_X = 60
 
 
 # Purge all articles from these aggregators
@@ -44,10 +41,10 @@ OBSOLETE_AGGREGATORS = ['Freakonometrics', 'deBoer', 'MoneyIllusion', 'Faunalyti
 PRIORITY_WEIGHT = 10   # The lower this number, the more links will be ranked according
                        # to the manual preferences set in `AGGREGATOR_WEIGHTS` below.
 
-TIME_WEIGHT = 10       # The lower this number, the more it will be the case that recent
+TIME_WEIGHT = 12       # The lower this number, the more it will be the case that recent
                        # links show up first
 
-RANDOM_WEIGHT = 35     # The lower this number, the more it will be the case that links will
+RANDOM_WEIGHT = 40     # The lower this number, the more it will be the case that links will
                        # show up in a random order, disregarding recenty or aggregator weights
 
 # Equation for ranking = 1 + (aggregator weight / PRIORITY_WEIGHT) +
@@ -60,12 +57,16 @@ AGGREGATOR_WEIGHTS = Case(When(Q(aggregator__exact='Dispatch'), then=100),
                           When(Q(aggregator__exact='SafeAI'), then=8),
                           When(Q(aggregator__exact='Bollard'), then=8),
                           When(Q(aggregator__exact='EALondon'), then=8),
-                          When(Q(aggregator__exact='ChinAI'), then=8),
-                          When(Q(aggregator__exact='ImportAI'), then=8),
-                          When(Q(aggregator__exact='MLSafety'), then=8),
-                          When(Q(aggregator__exact='FP21'), then=8),
-                          When(Q(aggregator__exact='FPChina'), then=8),
-                          When(Q(aggregator__exact='GCRPolicy'), then=8),
+                          When(Q(aggregator__exact='NavigatingAI'), then=8),
+                          When(Q(aggregator__exact='Yglesias'), then=4),
+                          When(Q(aggregator__exact='SSC'), then=4),
+                          When(Q(aggregator__exact='Noah'), then=4),
+                          When(Q(aggregator__exact='SplitTicket'), then=4),
+                          When(Q(aggregator__exact='Holden'), then=4),
+                          When(Q(aggregator__exact='DeNeufville'), then=4),
+                          When(Q(aggregator__exact='AI Impacts'), then=4),
+                          When(Q(aggregator__exact='Rodney'), then=4),
+                          When(Q(aggregator__exact='PoliticalKiwi'), then=4),
+                          When(Q(aggregator__exact='Steinhardt'), then=4),
                           default=1,
                           output_field=FloatField())
-
