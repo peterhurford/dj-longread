0c7707b7248cd63bf67b89a5f5e4c09a1da0feec
Peter Wildeford <peter@peterhuford.com>
2023-11-25 15:28:47 -0500

    purge more

--- link/config.py
+++ link/config.py
@@ -13,7 +13,7 @@ LONG_PURGE_OLDER_THAN_X = 60
 
 
 # Purge all articles from these aggregators
-OBSOLETE_AGGREGATORS = ['UnderSun']
+OBSOLETE_AGGREGATORS = ['80K', 'Beeminder', 'CEA', 'Crawford', 'HLI', 'IFP', 'Intelligencer', 'NMA', 'ThingOfThings', 'VanNostrand']
 
 # Metaweights
 PRIORITY_WEIGHT = 10   # The lower this number, the more links will be ranked according
