7ccc8037eb1331822e98da4925c7ce4b37d42676
Peter Wildeford <peter@peterhuford.com>
2024-01-22 18:16:28 +0100

    fix sql

--- utils/sql.py
+++ utils/sql.py
@@ -10,8 +10,43 @@ ALL_COLS = ['id', 'url', 'title', 'summary', 'domain', 'added', 'modified',
             'liked', 'category', 'aggregator', 'seed', 'tweet', 'starred']
 
 
-def fix_tweet(t):
-    return 0 if str(t) == '\\N' else str(t).split('.')[0]
+def clean_str(txt):
+    if not isinstance(txt, str):
+        return txt
+    txt = txt.replace('&nbsp;', '')
+    txt = txt.replace('&ldquo;', '"')
+    txt = txt.replace('&rdquo;', '"')
+    txt = txt.replace('&lsquo;', '\'')
+    txt = txt.replace('&rsquo;', '\'')
+    txt = txt.replace('&mdash;', '-')
+    txt = txt.replace('&ndash;', '-')
+    for i in range(10):
+        txt = txt.replace('"""', '"')
+        txt = txt.replace('\'\'\'', '\'')
+    txt = txt.replace('n"t', 'n\'t')
+    txt = txt.replace('n""t', 'n\'t')
+    return txt
+
+
+def clean_links(links):
+    links.columns = ALL_COLS
+    links = links[links['id'].notnull()]   # Drop empty column
+    links['id'] = links['id'].astype(int)  # Fix float ID issue
+    links['seed'] = links['seed'].astype(float).fillna(0)
+    links['tweet'] = links['tweet'].apply(lambda x: 0 if (str(x) == '\\N' or str(x) == '') else str(x).split('.')[0]).astype(float)
+    links['liked'] = links['liked'].apply(lambda x: np.nan if (str(x) == '\\N' or str(x) == '') else str(x).split('.')[0]).astype(float)
+    for var in ['url', 'title', 'summary', 'domain', 'category', 'aggregator']:
+        links[var] = links[var].apply(clean_str)
+    links['title'] = links['title'].fillna('')
+    links['liked'] = links['liked'].fillna(np.nan)
+    return links.sort_values('id')
+
+
+def clean_input(val):
+    if np.issubdtype(type(val), np.integer) or np.issubdtype(type(val), np.floating) or isinstance(val, float):
+        return int(val)
+    else:
+        return str(val)
 
 
 def table_exists(cur, table_name):
@@ -43,6 +78,7 @@ def delete_row(cur, table_name, column_name, value):
 
 
 def update_row(cur, table_name, set_col, set_val, where_col, where_val):
+    set_val = clean_input(set_val)
     cur.execute('UPDATE {} SET {} = %s WHERE {} = %s'.format(table_name, set_col, where_col), (set_val, where_val))
     return None
 
@@ -55,38 +91,6 @@ def find_row(cur, table_name, col, value, n=1):
         return cur.fetchall()
     else:
         return ValueError('n must be 1 or many')
-
-
-def clean_str(txt):
-    if not isinstance(txt, str):
-        return txt
-    txt = txt.replace('&nbsp;', '')
-    txt = txt.replace('&ldquo;', '"')
-    txt = txt.replace('&rdquo;', '"')
-    txt = txt.replace('&lsquo;', '\'')
-    txt = txt.replace('&rsquo;', '\'')
-    txt = txt.replace('&mdash;', '-')
-    txt = txt.replace('&ndash;', '-')
-    for i in range(10):
-        txt = txt.replace('"""', '"')
-        txt = txt.replace('\'\'\'', '\'')
-    txt = txt.replace('n"t', 'n\'t')
-    txt = txt.replace('n""t', 'n\'t')
-    return txt
-
-
-def clean_links(links):
-    links.columns = ALL_COLS
-    links = links[links['id'].notnull()]   # Drop empty column
-    links['id'] = links['id'].astype(int)  # Fix float ID issue
-    links['seed'] = links['seed'].astype(float).fillna(0)
-    links['tweet'] = links['tweet'].apply(lambda x: 0 if (str(x) == '\\N' or str(x) == '') else str(x).split('.')[0]).astype(float)
-    links['liked'] = links['liked'].apply(lambda x: np.nan if (str(x) == '\\N' or str(x) == '') else str(x).split('.')[0]).astype(float)
-    for var in ['url', 'title', 'summary', 'domain', 'category', 'aggregator']:
-        links[var] = links[var].apply(clean_str)
-    links['title'] = links['title'].fillna('')
-    links['liked'] = links['liked'].fillna(np.nan)
-    return links.sort_values('id')
 
 
 def export_db(cur, outfile='data/export.csv', verbose=True):
@@ -138,3 +142,42 @@ def import_db(conn, infile='data/export.csv', verbose=True):
     cur.close()
     return None
 
+
+
+def clean_str(txt):
+    if not isinstance(txt, str):
+        return txt
+    txt = txt.replace('&nbsp;', '')
+    txt = txt.replace('&ldquo;', '"')
+    txt = txt.replace('&rdquo;', '"')
+    txt = txt.replace('&lsquo;', '\'')
+    txt = txt.replace('&rsquo;', '\'')
+    txt = txt.replace('&mdash;', '-')
+    txt = txt.replace('&ndash;', '-')
+    for i in range(10):
+        txt = txt.replace('"""', '"')
+        txt = txt.replace('\'\'\'', '\'')
+    txt = txt.replace('n"t', 'n\'t')
+    txt = txt.replace('n""t', 'n\'t')
+    return txt
+
+
+def clean_links(links):
+    links.columns = ALL_COLS
+    links = links[links['id'].notnull()]   # Drop empty column
+    links['id'] = links['id'].astype(int)  # Fix float ID issue
+    links['seed'] = links['seed'].astype(float).fillna(0)
+    links['tweet'] = links['tweet'].apply(lambda x: 0 if (str(x) == '\\N' or str(x) == '') else str(x).split('.')[0]).astype(float)
+    links['liked'] = links['liked'].apply(lambda x: np.nan if (str(x) == '\\N' or str(x) == '') else str(x).split('.')[0]).astype(float)
+    for var in ['url', 'title', 'summary', 'domain', 'category', 'aggregator']:
+        links[var] = links[var].apply(clean_str)
+    links['title'] = links['title'].fillna('')
+    links['liked'] = links['liked'].fillna(np.nan)
+    return links.sort_values('id')
+
+
+def clean_input(val):
+    if np.issubdtype(type(val), np.integer) or np.issubdtype(type(val), np.floating) or isinstance(val, float):
+        return int(val)
+    else:
+        return str(val)
