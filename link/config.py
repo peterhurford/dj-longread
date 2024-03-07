005508a601c389a746b1a334a197351e695b6c99
Peter Wildeford <peter@peterhuford.com>
2024-03-07 16:53:23 +0100

    purge

--- link/config.py
+++ link/config.py
@@ -16,7 +16,7 @@ LONG_PURGE_OLDER_THAN_X = 60
 
 
 # Purge all articles from these aggregators
-OBSOLETE_AGGREGATORS = []
+OBSOLETE_AGGREGATORS = ['Gleech', 'Lincicome']
 
 # Metaweights
 PRIORITY_WEIGHT = 18   # The lower this number, the more links will be ranked according
